4c576d1544666f31488e789f2f720123a2309779
Yaron Koren <yaron57@gmail.com>
2021-03-09 18:12:34 +0000

    Add zoom options for Gantt charts
    Change-Id: I3966ffa21a865463dba8c821bb6ff25029787fc0

--- resources/ext.flexdiagrams.gantt.js
+++ resources/ext.flexdiagrams.gantt.js
@@ -57,4 +57,56 @@
 	var ganttHandler = new dg.gantt();
 	ganttHandler.initialize();
 
+	function setGanttZoom( evt ) {
+		switch (evt.data) {
+			case "hours":
+				gantt.config.scales = [
+					{unit: "day", step: 1, format: "%d %F"},
+					{unit: "hour", step: 1, format: "%h"}
+				];
+				break;
+			case "days":
+				gantt.config.scales = [
+					{unit: "day", step: 1, format: "%d %M"}
+				];
+				break;
+			case "months":
+				gantt.config.scales = [
+					{unit: "month", step: 1, format: "%M %Y"}
+				];
+				break;
+			case "years":
+				gantt.config.scales = [
+					{unit: "year", step: 1, format: "%Y"}
+				];
+				break;
+		}
+		gantt.init('canvas');
+	}
+
+	var zoomLevels = [ 'hours', 'days', 'months', 'years' ];
+	var zoomLevelButtons = [];
+	for ( var i = 0; i < zoomLevels.length; i++ ) {
+		var zoomLevel = zoomLevels[i];
+		zoomLevelButtons.push( new OO.ui.ButtonOptionWidget( {
+			data: zoomLevel,
+			label: mw.message( 'flexdiagrams-gantt-' + zoomLevel ).text(),
+			selected: ( zoomLevel == 'days')
+		}) );
+	}
+
+	var buttonSelect = new OO.ui.ButtonSelectWidget( {
+		items: zoomLevelButtons
+	} );
+
+	buttonSelect.on('select', setGanttZoom );
+
+	var zoomLayout = new OO.ui.FieldLayout( buttonSelect, {
+		align: 'top',
+		label: mw.message( 'flexdiagrams-gantt-zoomlevel' ).text()
+	} );
+
+	$('#canvas').after( '<div id="ganttZoomInput"></div><br style="clear: both;" />' );
+	$('#ganttZoomInput').append( zoomLayout.$element );
+
 }( jQuery, mediaWiki, flexdiagrams ) );
