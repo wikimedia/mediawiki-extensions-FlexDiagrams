0254bb2daaa29892df272576710b40e25af83f36
Yaron Koren <yaron57@gmail.com>
2026-05-28 23:46:06 +0000

    Fix error in Phan filename
    Change-Id: I62ff1705f72d670509b325f983408309e1e56c9c

---
+++ .phan/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = array_merge(
+	$cfg['suppress_issue_types'],
+	[
+		// Suppress issue types that currently exist in the codebase.
+		'MediaWikiNoIssetIfDefined',
+		'PhanTypeMismatchReturnProbablyReal',
+		'PhanUndeclaredConstant',
+		'PhanUndeclaredMethod'
+	]
+);
+
+return $cfg;
